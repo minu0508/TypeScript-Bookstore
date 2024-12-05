94ed999165cb01270f5ebdd1a995ad2aaf5077cf
minu0508 <ssm9756@naver.com>
2024-12-05 15:05:37 +0900

    [CREATE] - dayjs 설치하여 새로운 날짜 포맷 추가
    [UPDATE]
    - 책 세부 조회할 시 데이터를 가져온 화면에서 지저분하지 않도록 CSS 코드 수정 완료

--- src/pages/Book/Book.style.ts
+++ src/pages/Book/Book.style.ts
@@ -15,4 +15,39 @@ export const BookFillter = styled.div`
   padding: 20px 0;
 `;
 
-export const BookDetailStyle = styled.div``;
+export const BookDetailStyle = styled.div`
+  .header {
+    display: flex;
+    align-items: start;
+    gap: 24px;
+    padding: 0 0 24px 0;
+
+    .img {
+      flex: 1;
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+    .info {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+
+      dl {
+        display: flex;
+        margin: 0;
+
+        dt {
+          width: 80px;
+          color: ${({ theme }) => theme.color.secondary};
+        }
+
+        a {
+          color: ${({ theme }) => theme.color.primary};
+        }
+      }
+    }
+  }
+`;
--- src/pages/Book/BookDetail.tsx
+++ src/pages/Book/BookDetail.tsx
@@ -1,6 +1,44 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import * as S from './Book.style';
 import { useBook } from '../../hooks/useBook';
+import { getImgSrc } from '../../utils/image';
+import { Title } from '../../components/atoms/Title';
+import { BookDetail as IBookDetail } from '../../models/book.model';
+import { formatDate, formatNumber } from '../../utils/format';
+
+const bookInfoList = [
+  {
+    label: '카테고리',
+    key: 'categoryName',
+    filter: (book: IBookDetail) => <Link to={`/books?category_id=${book.category_id}`}>{book.categoryName}</Link>,
+  },
+  {
+    label: '포맷',
+    key: 'form',
+  },
+  {
+    label: '페이지',
+    key: 'pages',
+  },
+  {
+    label: 'ISBN',
+    key: 'isbn',
+  },
+  {
+    label: '출간일',
+    key: 'pubDate',
+    filter: (book: IBookDetail) => {
+      return formatDate(book.pubDate);
+    },
+  },
+  {
+    label: '가격',
+    key: 'price',
+    filter: (book: IBookDetail) => {
+      return `${formatNumber(book.price)} 원`;
+    },
+  },
+];
 
 export const BookDetail = () => {
   const { bookId } = useParams();
@@ -8,5 +46,28 @@ export const BookDetail = () => {
 
   if (!book) return null;
 
-  return <S.BookDetailStyle>{book.title}</S.BookDetailStyle>;
+  return (
+    <S.BookDetailStyle>
+      <header className="header">
+        <div className="img">
+          <img src={getImgSrc(book.img)} alt={book.title} />
+        </div>
+        <div className="info">
+          <Title size="large" color="text">
+            {book.title}
+          </Title>
+          {bookInfoList.map((item) => (
+            <dl>
+              <dt>{item.label}</dt>
+              <dd>{item.filter ? item.filter(book) : book[item.key as keyof IBookDetail]}</dd>
+            </dl>
+          ))}
+          <p className="summary">{book.summary}</p>
+          <div className="like">라이크</div>
+          <div className="add-cart">장바구니 넣기</div>
+        </div>
+      </header>
+      <div className="content"></div>
+    </S.BookDetailStyle>
+  );
 };
